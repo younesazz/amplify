4b87d05e845a486862a90a5e73935810896fc89e
younesazz <youneshamani88@gmail.com>
2022-10-23 14:18:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { withAuthenticator, Alert } from '@aws-amplify/ui-react';
 
-function App() {
+Amplify.configure(awsmobile)
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hey there {user.username}</h2>
+        <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
